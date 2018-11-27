4a31d299ad3018d15727375d98c3303fb1f66377
sammy00 <xiangminli@outlook.com>
2018-11-27 20:25:25 +0800

    Listing 4-23. Using the Identity Operator

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,7 +1,7 @@
 let firstVal = 5
 let secondVal = '5'
 
-if (firstVal == secondVal) {
+if (firstVal === secondVal) {
   console.log('They are the same')
 } else {
   console.log('They are NOT the same')
