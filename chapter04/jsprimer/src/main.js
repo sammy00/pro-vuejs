c3f87616f0b30197c3d9931a94801b54cb1d45e1
sammy00 <xiangminli@outlook.com>
2018-11-27 20:36:08 +0800

    Listing 4-34. Processing an Array

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,4 +1,12 @@
-let myArray = [100, 'Adam', true]
-let myOtherArray = [200, 'Bob', false, ...myArray]
+let products = [
+  { name: 'Hat', price: 24.5, stock: 10 },
+  { name: 'Kayak', price: 289.99, stock: 1 },
+  { name: 'Soccer Ball', price: 10, stock: 0 },
+  { name: 'Running Shoes', price: 116.5, stock: 20 },
+]
 
-myOtherArray.forEach((value, index) => console.log(`Index ${index}: ${value}`))
+let totalValue = products
+  .filter((item) => item.stock > 0)
+  .reduce((prev, item) => prev + item.price * item.stock, 0)
+
+console.log(`Total value: $${totalValue.toFixed(2)}`)
