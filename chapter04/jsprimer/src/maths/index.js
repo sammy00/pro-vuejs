4170e66a991606215468d6e8bb14b26954d15ff1
sammy00 <xiangminli@outlook.com>
2018-11-27 20:50:12 +0800

    Listing 4-50. Adding a Function in the index.js File

--- chapter04/jsprimer/src/maths/index.js
+++ chapter04/jsprimer/src/maths/index.js
@@ -6,3 +6,11 @@ export function mean(values) {
 
 export { addition }
 export * from './operations'
+
+export function asyncAdd(values) {
+  setTimeout(() => {
+    let total = addition(values)
+    console.log(`Async Total: ${total}`)
+    return total
+  }, 500)
+}
