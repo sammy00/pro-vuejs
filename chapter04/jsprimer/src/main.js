f64721c1591483b38f6b4043f48526bd57d85293
sammy00 <xiangminli@outlook.com>
2018-11-27 20:15:45 +0800

    Listing 4-11. Returning a Result from a Function

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,9 +1,4 @@
-function myFunc(name, weather, ...extraArgs) {
-  console.log('Hello' + name + '.')
-  console.log('It is' + weather + 'today.')
-  //for (let i = 0; i < extraArgs.length; i++) {
-  for (let extraArg of extraArgs) {
-    console.log('Extra Arg: ' + extraArg)
-  }
+function myFunc(name) {
+  return 'Hello ' + name + '.'
 }
-myFunc('Adam', 'sunny', 'one', 'two', 'three')
+console.log(myFunc('Adam'))
