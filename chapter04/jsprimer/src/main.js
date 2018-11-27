677f04a52e77b33ea80003609728fbf58c663b69
sammy00 <xiangminli@outlook.com>
2018-11-27 20:53:05 +0800

    Listing 4-53. Observing a Promise

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,6 +1,5 @@
 import { asyncAdd } from './maths'
 
 let values = [10, 20, 30, 40, 50]
-let total = asyncAdd(values)
 
-console.log(`Main Total: ${total}`)
+asyncAdd(values).then((total) => console.log(`Main Total: ${total}`))
