1812002edf55f3ff144050c7ecfb9c25c074387f
sammy00 <xiangminli@outlook.com>
2018-11-27 20:44:45 +0800

    Listing 4-44. Using a Module

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,6 +1,8 @@
 import additionFunction from './maths/sum'
+import { multiply, subtract } from './maths/operations'
 
 let values = [10, 20, 30, 40, 50]
-let total = additionFunction(values)
 
-console.log(`Total: ${total}`)
+console.log(`Sum: ${additionFunction(values)}`)
+console.log(`Multiply: ${multiply(values)}`)
+console.log(`Subtract: ${subtract(1000, values)}`)
