0e210a4c2e5c60c57eebdd25c454008dcaf656b2
sammy00 <xiangminli@outlook.com>
2018-11-27 20:20:32 +0800

    Listing 4-16. Using var to Declare Variables

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,10 +1,10 @@
 function messageFunction(name, weather) {
-  let message = 'Hello, Adam'
+  var message = 'Hello, Adam'
   if (weather == 'sunny') {
-    let message = 'It is a nice day'
+    var message = 'It is a nice day'
     console.log(message)
   } else {
-    let message = 'It is ' + weather + ' today'
+    var message = 'It is ' + weather + ' today'
     console.log(message)
   }
   console.log(message)
