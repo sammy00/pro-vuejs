b3569d2fc90963a31243cdb878d4c9a482d461f2
sammy00 <xiangminli@outlook.com>
2018-11-27 13:32:05 +0800

    displaying the list of tasks

--- chapter01/todo/src/App.vue
+++ chapter01/todo/src/App.vue
@@ -5,6 +5,22 @@
         <a href="#" class="brand-logo center">{{name}}'s To Do List</a>
       </div>
     </nav>
+    <main class="container">
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="row" v-for="t in tasks" v-bind:key="t.action">
+            <td>{{t.action}}</td>
+            <td>{{t.done}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
   </div>
 </template>
 
@@ -12,7 +28,15 @@
 export default {
   name: "app",
   data() {
-    return { name: "Adam" };
+    return {
+      name: "Adam",
+      tasks: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }
+      ]
+    };
   }
 };
 </script>
