8ccb4667dd8b2ed0e1c23d5d232b8d83dbfa11dc
sammy00 <xiangminli@outlook.com>
2018-11-27 21:37:52 +0800

    Listing 5-10. Adding the Vuex Data Store in the main.js

--- chapter05/sportsstore/src/main.js
+++ chapter05/sportsstore/src/main.js
@@ -6,6 +6,9 @@ Vue.config.productionTip = false
 import 'materialize-css/dist/css/materialize.min.css'
 import 'material-design-icons/iconfont/material-icons.css'
 
+import store from './store'
+
 new Vue({
   render: (h) => h(App),
+  store,
 }).$mount('#app')
