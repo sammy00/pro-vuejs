3fa7ca0df5e60cebed7323387ffa18b9f24caf52
sammy00 <xiangminli@outlook.com>
2018-11-27 20:26:56 +0800

    Listing 4-25. Using the number.toString Method

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,5 +1,3 @@
-let myData1 = 5 + 5
-let myData2 = 5 + '5'
+let myData1 = (5).toString() + String(5)
 
-console.log('Result 1: ' + myData1)
-console.log('Result 2: ' + myData2)
+console.log('Result: ' + myData1)
