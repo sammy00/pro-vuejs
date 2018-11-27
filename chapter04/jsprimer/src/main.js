413745c304e6cd6e4caeafdece1015dac14f710b
sammy00 <xiangminli@outlook.com>
2018-11-27 20:24:53 +0800

    Listing 4-22. Using the Equality Operator

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,19 +1,8 @@
-let name = 'Adam'
-if (name == 'Adam') {
-  console.log('Name is Adam')
-} else if (name == 'Jacqui') {
-  console.log('Name is Jacqui')
+let firstVal = 5
+let secondVal = '5'
+
+if (firstVal == secondVal) {
+  console.log('They are the same')
 } else {
-  console.log('Name is neither Adam or Jacqui')
-}
-switch (name) {
-  case 'Adam':
-    console.log('Name is Adam')
-    break
-  case 'Jacqui':
-    console.log('Name is Jacqui')
-    break
-  default:
-    console.log('Name is neither Adam or Jacqui')
-    break
+  console.log('They are NOT the same')
 }
