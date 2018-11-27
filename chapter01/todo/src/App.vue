d3ac08923338ba8838b1aac4ff7335126d4397b1
sammy00 <xiangminli@outlook.com>
2018-11-27 19:31:30 +0800

    filtering completed tasks

--- chapter01/todo/src/App.vue
+++ chapter01/todo/src/App.vue
@@ -6,28 +6,38 @@
       </div>
     </nav>
     <main class="container">
-      <table>
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Done</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="row" v-for="t in tasks" v-bind:key="t.action">
-            <td>{{t.action}}</td>
-            <!--<td>{{t.done}}</td>-->
-            <td>
-              <form action="#">
-                <label>
-                  <input type="checkbox" v-model="t.done">
-                  <span>{{t.done}}</span>
-                </label>
-              </form>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="row">
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+              <td>{{t.action}}</td>
+              <!--<td>{{t.done}}</td>-->
+              <td>
+                <form action="#">
+                  <label>
+                    <input type="checkbox" v-model="t.done">
+                    <span>{{t.done}}</span>
+                  </label>
+                </form>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="row">
+        <form action="#" class="center">
+          <label>
+            <input type="checkbox" v-model="hideCompleted">
+            <span>Hide completed tasks</span>
+          </label>
+        </form>
+      </div>
     </main>
   </div>
 </template>
@@ -43,8 +53,14 @@ export default {
         { action: "Get Shoes", done: false },
         { action: "Collect Tickets", done: true },
         { action: "Call Joe", done: false }
-      ]
+      ],
+      hideCompleted: true
     };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks;
+    }
   }
 };
 </script>
