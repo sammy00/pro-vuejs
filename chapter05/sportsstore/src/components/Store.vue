241ec13705fa3459c1272f72807bba9de38ed64e
sammy00 <xiangminli@outlook.com>
2018-11-27 21:48:43 +0800

    Listing 5-13. The Contents of the ProductList.vue

--- chapter05/sportsstore/src/components/Store.vue
+++ chapter05/sportsstore/src/components/Store.vue
@@ -1,16 +1,16 @@
 <template>
   <main>
     <nav>
-      <div class="nav-wrapper">
+      <div class="nav-wrapper container">
         <a href="#" class="brand-logo">SPORTS STORE</a>
       </div>
     </nav>
     <div class="container">
       <div class="row">
-        <div class="col s3 grey lighten-2">
+        <div class="col s3 cyan">
           <h4 class="white-text">Categories</h4>
         </div>
-        <div class="col s9 grey lighten-4">
+        <div class="col s9 green">
           <h4 class="white-text">Products</h4>
         </div>
       </div>
