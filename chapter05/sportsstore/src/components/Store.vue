67727477326963e9581485ff6691b5ce11ffc94c
sammy00 <xiangminli@outlook.com>
2018-11-27 21:43:49 +0800

    Listing 5-11. The Contents of the Store.vue

---
+++ chapter05/sportsstore/src/components/Store.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">SPORTS STORE</a>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col s3 grey lighten-2">
+          <h4 class="white-text">Categories</h4>
+        </div>
+        <div class="col s9 grey lighten-4">
+          <h4 class="white-text">Products</h4>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
