001d50cc4249c832c08910fe05c11c24729b9634
sammy00 <xiangminli@outlook.com>
2018-11-27 20:45:28 +0800

    Listing 4-45. Using a Module Alias

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,8 +1,8 @@
 import additionFunction from './maths/sum'
-import { multiply, subtract } from './maths/operations'
+import { multiply, subtract as minus } from './maths/operations'
 
 let values = [10, 20, 30, 40, 50]
 
 console.log(`Sum: ${additionFunction(values)}`)
 console.log(`Multiply: ${multiply(values)}`)
-console.log(`Subtract: ${subtract(1000, values)}`)
+console.log(`Subtract: ${minus(1000, values)}`)
