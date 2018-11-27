df393efa7e50294bf715ee5f71fe3c88e021aa06
sammy00 <xiangminli@outlook.com>
2018-11-27 20:53:59 +0800

    Listing 4-54. Using async and await

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -2,4 +2,9 @@ import { asyncAdd } from './maths'
 
 let values = [10, 20, 30, 40, 50]
 
-asyncAdd(values).then((total) => console.log(`Main Total: ${total}`))
+async function doTask() {
+  let total = await asyncAdd(values)
+  console.log(`Main Total: ${total}`)
+}
+
+doTask()
