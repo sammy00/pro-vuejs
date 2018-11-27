9f786121d5f25eac9cf998004a32970caeebb9eb
sammy00 <xiangminli@outlook.com>
2018-11-27 20:48:01 +0800

    Listing 4-48. Importing an Entire Module

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,8 +1,8 @@
-import additionFunction from './maths/sum'
-import * as ops from './maths/operations'
+import * as math from './maths'
 
 let values = [10, 20, 30, 40, 50]
 
-console.log(`Sum: ${additionFunction(values)}`)
-console.log(`Multiply: ${ops.multiply(values)}`)
-console.log(`Subtract: ${ops.subtract(1000, values)}`)
+console.log(`Sum: ${math.addition(values)}`)
+console.log(`Multiply: ${math.multiply(values)}`)
+console.log(`Subtract: ${math.subtract(1000, values)}`)
+console.log(`Mean: ${math.mean(values)}`)
