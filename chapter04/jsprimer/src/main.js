af9b04d68de9d9a6fe1339f8951f5d4dc3ccf294
sammy00 <xiangminli@outlook.com>
2018-11-27 20:14:53 +0800

    Listing 4-10. Using a Rest Parameter

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,5 +1,9 @@
-function myFunc(name, weather = 'raining') {
-  console.log('Hello ' + name + '.')
-  console.log('It is ' + weather + ' today.')
+function myFunc(name, weather, ...extraArgs) {
+  console.log('Hello' + name + '.')
+  console.log('It is' + weather + 'today.')
+  //for (let i = 0; i < extraArgs.length; i++) {
+  for (let extraArg of extraArgs) {
+    console.log('Extra Arg: ' + extraArg)
+  }
 }
-myFunc('Adam')
+myFunc('Adam', 'sunny', 'one', 'two', 'three')
