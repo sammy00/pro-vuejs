c760a5800cefa8b48ba026b27c92084047a7a70d
sammy00 <xiangminli@outlook.com>
2018-11-27 20:09:32 +0800

    Listing 4-6. Defining a JavaScript Function

--- chapter04/jsprimer/src/main.js
+++ chapter04/jsprimer/src/main.js
@@ -1,14 +1,5 @@
-/*
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
-*/
-console.log('Hello')
-console.log('Apples')
-console.log('This is a statement')
-console.log('This is also a statement')
+const myFunc = function() {
+  console.log('This statement is inside the function')
+}
+console.log('This statement is outside the function')
+myFunc()
