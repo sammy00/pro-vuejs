b2b2df338edc592803b158e40cd95da76d9214e3
sammy00 <xiangminli@outlook.com>
2018-11-30 22:00:38 +0800

    Listing 5-17. The Contents of the PageControls.vue

---
+++ chapter05/sportsstore/src/components/PageControls.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <ul class="pagination">
+      <!--
+      <li v-bind:class="{ 'disabled': 1==currentPage}">
+        <a href="#!">
+          <i class="material-icons">chevron_left</i>
+        </a>
+      </li>
+      -->
+      <li
+        v-for="i in pageNumbers"
+        v-bind:key="i"
+        class="waves-effect"
+        v-bind:class="{ 'active': i==currentPage}"
+      >{{ i }}</li>
+      <!--
+      <li class="waves-effect">
+        <a href="#!">
+          <i class="material-icons">chevron_right</i>
+        </a>
+      </li>
+      -->
+    </ul>
+  </div>
+
+  <!--
+  <div v-if="pageCount > 1" class="text-right">
+    <div class="btn-group mx-2">
+      <button
+        v-for="i in pageNumbers"
+        v-bind:key="i"
+        class="btn btn-secpmdary"
+        v-bind:class="{ 'btn-primary': i == currentPage }"
+      >{{ i }}</button>
+    </div>
+  </div>
+  -->
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["currentPage"]),
+    ...mapGetters(["pageCount"]),
+    pageNumbers() {
+      return [...Array(this.pageCount + 1).keys()].slice(1);
+    }
+  }
+};
+</script>
