98142eac97fdbe8090313d446472420451e820b9
sammy00 <xiangminli@outlook.com>
2018-12-13 18:15:18 +0800

    Listing 6-12. Initializing the Cart in the App.vue

--- chapter06/sportsstore/src/App.vue
+++ chapter06/sportsstore/src/App.vue
@@ -12,10 +12,15 @@ export default {
   name: "app",
   //components: { Store },
   methods: {
-    ...mapActions(["getData"])
+    //...mapActions(["getData"])
+    ...mapActions({
+      getData: "getData",
+      initializeCart: "cart/initializeCart"
+    })
   },
   created() {
     this.getData();
+    this.initializeCart(this.$store);
   }
 };
 </script>
