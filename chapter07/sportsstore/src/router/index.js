f00a7ebb812998e3f75b32bc5af2bf52954334e2
sammy00 <xiangminli@outlook.com>
2018-12-18 11:55:32 +0800

    Listing 7-13. Adding Routes in the index.js

--- chapter07/sportsstore/src/router/index.js
+++ chapter07/sportsstore/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Admin from '../components/admin/Admin'
+import Authentication from '../components/admin/Authentication'
 import Checkout from '../components/Checkout'
 import OrderThanks from '../components/OrderThanks'
 import Store from '../components/Store'
@@ -12,8 +14,10 @@ export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Store },
+    { path: '/admin', component: Admin },
     { path: '/cart', component: ShoppingCart },
     { path: '/checkout', component: Checkout },
+    { path: '/login', component: Authentication },
     { path: '/thanks/:id', component: OrderThanks },
     { path: '*', redirect: '/' },
   ],
