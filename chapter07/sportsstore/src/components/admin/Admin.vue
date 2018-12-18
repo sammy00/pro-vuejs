08d6e6fc09eb87c574054389bdca9c105b2c628e
sammy00 <xiangminli@outlook.com>
2018-12-18 13:18:27 +0800

    Listing 7-19. Adding Features in the orders.js

--- chapter07/sportsstore/src/components/admin/Admin.vue
+++ chapter07/sportsstore/src/components/admin/Admin.vue
@@ -6,11 +6,11 @@
     <v-container fluid grid-list-md>
       <v-layout row wrap>
         <v-flex col sm12 md3>
-          <category-controls/>
+          <v-btn to="/admin/products" block color="primary">Products</v-btn>
+          <v-btn to="/admin/orders" block color="primary">Orders</v-btn>
         </v-flex>
         <v-flex col sm12 md9>
-          <search/>
-          <product-list/>
+          <router-view/>
         </v-flex>
       </v-layout>
     </v-container>
