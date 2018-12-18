6362cba6c98d459c071d9a00697c0a854db17776
sammy00 <xiangminli@outlook.com>
2018-12-19 07:48:39 +0800

    Listing 8-9. Removing Credentials in the Authentication.vue

--- chapter08/sportsstore/src/components/admin/Authentication.vue
+++ chapter08/sportsstore/src/components/admin/Authentication.vue
@@ -33,8 +33,8 @@ export default {
   components: { ValidationError },
   data: function() {
     return {
-      username: "admin",
-      password: "secret",
+      username: null,
+      password: null,
       show: false,
       showFailureMessage: false
     };
