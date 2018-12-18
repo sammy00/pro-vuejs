d5e1f515b389dfe3b105a7052ed9ce66ae8f1c02
sammy00 <xiangminli@outlook.com>
2018-12-18 12:38:20 +0800

    Listing 7-14. Adding a Route Guard in the index.js

--- chapter07/sportsstore/src/router/index.js
+++ chapter07/sportsstore/src/router/index.js
@@ -8,13 +8,25 @@ import OrderThanks from '../components/OrderThanks'
 import Store from '../components/Store'
 import ShoppingCart from '../components/ShoppingCart'
 
+import dataStore from '../store'
+
 Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Store },
-    { path: '/admin', component: Admin },
+    {
+      path: '/admin',
+      component: Admin,
+      beforeEnter(to, from, next) {
+        if (dataStore.state.auth.authenticated) {
+          next()
+        } else {
+          next('/login')
+        }
+      },
+    },
     { path: '/cart', component: ShoppingCart },
     { path: '/checkout', component: Checkout },
     { path: '/login', component: Authentication },
