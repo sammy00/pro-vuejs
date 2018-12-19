587f7d071b3a75840454960e45dd125cdbe2b0a7
sammy00 <xiangminli@outlook.com>
2018-12-19 12:17:48 +0800

    Listing 9-16. Specifying an HTML File in the App.vue

--- chapter09/nomagic/src/App.vue
+++ chapter09/nomagic/src/App.vue
@@ -1,9 +1,4 @@
-<template>
-  <div class="card-panel grey">
-    <h1 class="white-text">{{ message }}</h1>
-    <button class="btn grey" @click="handleClick">Press Me</button>
-  </div>
-</template>
+<template src="./App.html" />
 
 <script>
 export default {
