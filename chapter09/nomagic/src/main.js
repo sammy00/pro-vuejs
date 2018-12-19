6548cc400971f55fdd39f75e995a3145e6870740
sammy00 <xiangminli@outlook.com>
2018-12-19 12:01:39 +0800

    Listing 9-10. Displaying a Conditional Message in the main.js

--- chapter09/nomagic/src/main.js
+++ chapter09/nomagic/src/main.js
@@ -17,6 +17,14 @@ new Vue({
   data: {
     counter: 0,
   },
+
+  computed: {
+    message() {
+      return 0 == this.counter
+        ? 'Button Not Pressed'
+        : `Button Presses: ${this.counter}`
+    },
+  },
   methods: {
     handleClick() {
       this.counter++
@@ -24,7 +32,7 @@ new Vue({
   },
   template: `<div class="card-panel grey">
     <h1 class="white-text">
-      Button Presses: {{ counter }}
+      {{ message }}
     </h1>
     <button class="btn grey" @click="handleClick"> Press Me </button>
   </div>`,
