1a9279eb3d692ce8bc182577a5a030160a592322
sammy00 <xiangminli@outlook.com>
2018-12-24 12:56:51 +0800

    Listing 11-8. Displaying a Data Value in the App.vue

--- chapter11/templatesanddata/src/App.vue
+++ chapter11/templatesanddata/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="card-panel blue white-text">
-    <h3 class="center-align">This is the component</h3>
+    <h3 class="center-align">Product: {{ name }}</h3>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MyComponent"
+  name: "MyComponent",
+  data: function() {
+    return {
+      name: "Kayak"
+    };
+  }
 };
 </script>
 
