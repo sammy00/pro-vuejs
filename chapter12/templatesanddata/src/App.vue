24049a1c45c3d491a48037fa879f1a55890aa67f
sammy00 <xiangminli@outlook.com>
2018-12-24 13:31:51 +0800

    Listing 12-7. Applying the Same Directive to Peer Elements in the App.vue

--- chapter12/templatesanddata/src/App.vue
+++ chapter12/templatesanddata/src/App.vue
@@ -5,7 +5,14 @@
         Product:
         <span v-text="name"></span>
       </h3>
-      <h4 v-if="showElements" class="center-align">{{ price }}</h4>
+      <ul class="collection with-header black-text">
+        <li class="collection-header">
+          <h4>List item</h4>
+        </li>
+        <li v-if="showElements" class="collection-item">{{name}}</li>
+        <li v-if="showElements" class="collection-item">{{price}}</li>
+        <li class="collection-item">Other list item</li>
+      </ul>
     </div>
     <div class="center-align">
       <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
