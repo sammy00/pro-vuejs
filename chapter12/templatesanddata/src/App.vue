f5ce84696fc0c21c04383a986ccfeb1e655d3a9f
sammy00 <xiangminli@outlook.com>
2018-12-24 13:23:45 +0800

    Listing 12-4. Adding a Data Property in the App.vue

--- chapter12/templatesanddata/src/App.vue
+++ chapter12/templatesanddata/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="container-fluid">
     <div class="card-panel blue white-text">
-      <h3 class="center-align">Product:
+      <h3 class="center-align">
+        Product:
         <span v-text="name"></span>
       </h3>
+      <p v-text="fragment" class="center-align"></p>
     </div>
     <div class="center-align">
       <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
@@ -16,7 +18,11 @@ export default {
   name: "MyComponent",
   data: function() {
     return {
-      name: "Lifejacket"
+      name: "Lifejacket",
+      fragment: `<div class="form-group">
+Password
+<input class="form-control" />
+</div>`
     };
   },
   methods: {
