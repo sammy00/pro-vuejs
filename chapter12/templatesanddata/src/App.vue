365c312d7fd30c6e5dde6503a63bf03605544348
sammy00 <xiangminli@outlook.com>
2018-12-24 13:34:54 +0800

    Listing 12-10. Simplifying Content Selection in the App.vue

--- chapter12/templatesanddata/src/App.vue
+++ chapter12/templatesanddata/src/App.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="card-panel blue white-text">
       <h3 v-if="showElements" class="center-align">Product: {{ name }}</h3>
-      <h3 v-if="!showElements" class="center-align">Price: {{price}}</h3>
+      <h3 v-else class="center-align">Price: {{price}}</h3>
     </div>
     <div class="center-align">
       <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
