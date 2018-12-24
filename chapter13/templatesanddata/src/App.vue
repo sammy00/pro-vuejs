58fef104eaa19cbde05986bc6b52771e834690f9
sammy00 <xiangminli@outlook.com>
2018-12-24 18:49:27 +0800

    Listing 13-4. Using the v-for Alias in the App.vue

--- chapter13/templatesanddata/src/App.vue
+++ chapter13/templatesanddata/src/App.vue
@@ -3,18 +3,22 @@
     <div class="card-panel blue">
       <h3 class="white-text center-align">Products</h3>
     </div>
-    <table class="striped">
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-      </tr>
-      <tbody>
-        <tr v-for="p in products">
-          <td>Name</td>
-          <td>Category</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      <div class="col s10 offset-s1">
+        <table class="striped">
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+          <tbody>
+            <tr v-for="p in products">
+              <td>{{ p.name }}</td>
+              <td>{{ p.price | currency }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
     <div class="center-align">
       <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
@@ -33,6 +37,14 @@ export default {
         { name: "Soccer Ball", price: 19.5 }
       ]
     };
+  },
+  filters: {
+    currency(value) {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      }).format(value);
+    }
   },
 
   methods: {
