f45070169297abeb0b71d3e77572ab67a9117c0e
sammy00 <xiangminli@outlook.com>
2018-12-24 13:21:05 +0800

    Listing 12-1. Simplifying the Content of the App.vue

---
+++ chapter12/templatesanddata/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid">
+    <div class="card-panel blue white-text">
+      <h3 class="center-align">Product: {{ name }}</h3>
+    </div>
+    <div class="center-align">
+      <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data: function() {
+    return {
+      name: "Lifejacket"
+    };
+  },
+  methods: {
+    handleClick() {
+      // do nothing
+    }
+  }
+};
+</script>
+
