211cf438263fe8171c6e6145ada242f091e378a6
sammy00 <xiangminli@outlook.com>
2018-12-24 13:04:21 +0800

    Listing 11-12. Computing Similar Values in the App.vue

--- chapter11/templatesanddata/src/App.vue
+++ chapter11/templatesanddata/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="card-panel blue white-text">
     <h3 class="center-align">Product: {{ name }}</h3>
-    <h3 class="center-align">Price: ${{ totalPrice.toFixed(2) }}</h3>
+    <h4 class="center-align">Price: ${{ lowTotalPrice.toFixed(2) }} (Low Rate)</h4>
+    <h4 class="center-align">Price: ${{ highTotalPrice.toFixed(2) }} (High Rate)</h4>
   </div>
 </template>
 
@@ -12,13 +13,22 @@ export default {
     return {
       name: "Kayak",
       price: 275,
-      taxRate: 12
+      lowTaxRate: 12,
+      highTaxRate: 20
     };
   },
   computed: {
     totalPrice() {
       let tp = this.price + this.price * (this.taxRate / 100);
       console.log(`Calculated: ${tp} (${this.taxRate})`);
+      return tp;
+    },
+    lowTotalPrice() {
+      let tp = this.price + this.price * (this.lowTaxRate / 100);
+      return tp;
+    },
+    highTotalPrice() {
+      let tp = this.price + this.price * (this.highTaxRate / 100);
       return tp;
     }
   }
