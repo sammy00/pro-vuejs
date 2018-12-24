e8caab84d1ef893c14de0a23fba79517dfe7b48b
sammy00 <xiangminli@outlook.com>
2018-12-24 18:46:11 +0800

    Listing 13-3. Enumerating an Array in the App.vue

---
+++ chapter13/templatesanddata/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container-fluid">
+    <div class="card-panel blue">
+      <h3 class="white-text center-align">Products</h3>
+    </div>
+    <table class="striped">
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+      <tbody>
+        <tr v-for="p in products">
+          <td>Name</td>
+          <td>Category</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="center-align">
+      <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data: function() {
+    return {
+      products: [
+        { name: "Kayak", price: 275 },
+        { name: "Lifejacket", price: 48.95 },
+        { name: "Soccer Ball", price: 19.5 }
+      ]
+    };
+  },
+
+  methods: {
+    handleClick() {
+      // do nothing
+      this.highlight = !this.highlight;
+    }
+  }
+};
+</script>
+
+<style>
+[data-size="big"] {
+  font-size: 40pt;
+}
+[data-size="small"] {
+  font-size: 20pt;
+}
+</style>
