bbfe7128622960ce9c93b6271e1f677aec189504
sammy00 <xiangminli@outlook.com>
2018-12-24 13:08:13 +0800

    Listing 11-14. Calling Methods Directly in the App.vue

--- chapter11/templatesanddata/src/App.vue
+++ chapter11/templatesanddata/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card-panel blue white-text">
     <h3 class="center-align">Product: {{ name }}</h3>
-    <h4 class="center-align">Price: ${{ lowTotalPrice.toFixed(2) }} (Low Rate)</h4>
-    <h4 class="center-align">Price: ${{ highTotalPrice.toFixed(2) }} (High Rate)</h4>
+    <h4 class="center-align">Price: ${{ getTotalPrice(lowTaxRate).toFixed(2) }} (Low Rate)</h4>
+    <h4 class="center-align">Price: ${{ getTotalPrice(highTaxRate).toFixed(2) }} (High Rate)</h4>
   </div>
 </template>
 
@@ -16,14 +16,6 @@ export default {
       lowTaxRate: 12,
       highTaxRate: 20
     };
-  },
-  computed: {
-    lowTotalPrice() {
-      return this.getTotalPrice(this.lowTaxRate);
-    },
-    highTotalPrice() {
-      return this.getTotalPrice(this.highTaxRate);
-    }
   },
   methods: {
     getTotalPrice(taxRate) {
