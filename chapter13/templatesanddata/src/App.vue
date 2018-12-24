895666202aa43b16ddc998262ff103f82e7bfe5d
sammy00 <xiangminli@outlook.com>
2018-12-24 19:11:12 +0800

    Listing 13-15. Paging Data in the App.vue

--- chapter13/templatesanddata/src/App.vue
+++ chapter13/templatesanddata/src/App.vue
@@ -7,15 +7,11 @@
       <div class="col s10 offset-s1">
         <table class="stripped">
           <tr>
-            <th>Index</th>
-            <th>Key</th>
             <th>Name</th>
             <th>Price</th>
           </tr>
           <tbody>
-            <tr v-for="(p, key, i) in products" :key="p.name">
-              <td>{{ i }}</td>
-              <td>{{ key }}</td>
+            <tr v-for="p in pageItems" :key="p.name">
               <td>{{ p.name }}</td>
               <td>{{ p.price | currency }}</td>
             </tr>
@@ -25,12 +21,15 @@
     </div>
 
     <div class="center-align">
-      <!-- eslint-disable-next-line vue/require-v-for-key -->
-      <!--<button v-for="i in 5" v-on:click="handleClick(i)" class="btn btn-primary">{{ i }}</button>-->
       <ul class="pagination">
         <!-- eslint-disable-next-line vue/require-v-for-key -->
-        <li class="waves-effect" v-for="i in 5" :class="pagination == i ? 'active':''" :key="i">
-          <a @click="handleClick(i)">{{ i }}</a>
+        <li
+          class="waves-effect"
+          v-for="i in pageCount"
+          :class="{'active': currentPage == i}"
+          :key="i"
+        >
+          <a @click="selectPage(i)">{{ i }}</a>
         </li>
       </ul>
     </div>
@@ -44,14 +43,29 @@ export default {
   name: "MyComponent",
   data: function() {
     return {
-      products: {
-        kayak: { name: "Kayak", price: 275 },
-        22: { name: "Lifejacket", price: 48.95 },
-        3: { name: "Soccer Ball", price: 19.5 },
-        "4": { name: "Corner Flags", price: 39.95 }
-      },
-      pagination: 1
+      pageSize: 3,
+      currentPage: 1,
+      products: [
+        { name: "Kayak", price: 275 },
+        { name: "Lifejacket", price: 48.95 },
+        { name: "Soccer Ball", price: 19.5 },
+        { name: "Corner Flags", price: 39.95 },
+        { name: "Stadium", price: 79500 },
+        { name: "Thinking Cap", price: 16 },
+        { name: "Unsteady Chair", price: 29.95 },
+        { name: "Human Chess Board", price: 75 },
+        { name: "Bling Bling King", price: 1200 }
+      ]
     };
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.products.length / this.pageSize);
+    },
+    pageItems() {
+      let start = (this.currentPage - 1) * this.pageSize;
+      return this.products.slice(start, start + this.pageSize);
+    }
   },
   filters: {
     currency(value) {
@@ -61,10 +75,12 @@ export default {
       }).format(value);
     }
   },
-
   methods: {
     handleClick() {
       Vue.set(this.products, 5, { name: "Running Shoes", price: 100 });
+    },
+    selectPage(page) {
+      this.currentPage = page;
     }
   }
 };
