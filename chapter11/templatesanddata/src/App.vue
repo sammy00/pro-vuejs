f13b3b999f8115275e7b11ec149efe42329b2d24
sammy00 <xiangminli@outlook.com>
2018-12-24 12:59:07 +0800

    Listing 11-9. Using a More Complex Data Binding Expression in the App.vue

--- chapter11/templatesanddata/src/App.vue
+++ chapter11/templatesanddata/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="card-panel blue white-text">
     <h3 class="center-align">Product: {{ name }}</h3>
+    <h3 class="center-align">Price: ${{ (price + (price * (taxRate / 100))).toFixed(2) }}</h3>
   </div>
 </template>
 
@@ -9,7 +10,9 @@ export default {
   name: "MyComponent",
   data: function() {
     return {
-      name: "Kayak"
+      name: "Kayak",
+      price: 275,
+      taxRate: 12
     };
   }
 };
