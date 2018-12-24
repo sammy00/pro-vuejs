e8210d6f7aa02c3bae2ffa59e4527ef2dcef13e0
sammy00 <xiangminli@outlook.com>
2018-12-24 18:59:19 +0800

    Listing 13-12. Using an Object for Enumeration in the App.vue

--- chapter13/templatesanddata/src/App.vue
+++ chapter13/templatesanddata/src/App.vue
@@ -5,15 +5,15 @@
     </div>
     <div class="row">
       <div class="col s10 offset-s1">
-        <table>
+        <table class="stripped">
           <tr>
             <th>Index</th>
             <th>Name</th>
             <th>Price</th>
           </tr>
           <tbody>
-            <tr v-for="(p,i) in products" :key="p.name" :odd="i%2==0">
-              <td>{{ i+ 1 }}</td>
+            <tr v-for="(p, key, i) in products" :key="p.name">
+              <td>{{ i }}</td>
               <td>{{ p.name }}</td>
               <td>{{ p.price | currency }}</td>
             </tr>
@@ -35,14 +35,12 @@ export default {
   name: "MyComponent",
   data: function() {
     return {
-      products: [
-        { name: "Kayak", price: 275 },
-        { name: "Lifejacket", price: 48.95 },
-        { name: "Soccer Ball", price: 19.5 },
-        { name: "Corner Flags", price: 39.95 },
-        { name: "Stadium", price: 79500 },
-        { name: "Thinking Cap", price: 16 }
-      ]
+      products: {
+        1: { name: "Kayak", price: 275 },
+        2: { name: "Lifejacket", price: 48.95 },
+        3: { name: "Soccer Ball", price: 19.5 },
+        4: { name: "Corner Flags", price: 39.95 }
+      }
     };
   },
   filters: {
@@ -56,7 +54,7 @@ export default {
 
   methods: {
     handleClick() {
-      Vue.set(this.products, 1, { name: "Running Shoes", price: 100 });
+      Vue.set(this.products, 5, { name: "Running Shoes", price: 100 });
     }
   }
 };
