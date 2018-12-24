301e3848816e9fdcc10eac9038ddb58b9da7e972
sammy00 <xiangminli@outlook.com>
2018-12-24 13:38:25 +0800

    Listing 12-13. Leaving Invisible Elements in the DOM in the App

--- chapter12/templatesanddata/src/App.vue
+++ chapter12/templatesanddata/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-fluid">
     <div class="card-panel blue white-text">
-      <h3 v-if="counter % 2 == 0" class="center-align">Product: {{name}}</h3>
-      <h3 v-else class="center-align">Category: {{category}}</h3>
+      <h3 v-show="counter % 2 == 0" class="center-align">Product: {{name}}</h3>
+      <h3 v-show="counter % 2 != 0" class="center-align">Category: {{category}}</h3>
     </div>
     <div class="center-align">
       <button v-on:click="handleClick" class="btn btn-primary">Press Me</button>
