5d4a82c9be1988a4c1aae86718e19abf1c46fc58
sammy00 <xiangminli@outlook.com>
2019-01-02 13:17:08 +0800

    Listing 16-9. Adding a Prop in the Child.vue

--- chapter16/templatesanddata/src/components/Child.vue
+++ chapter16/templatesanddata/src/components/Child.vue
@@ -2,6 +2,7 @@
   <div class="card-panel blue white-text">
     <h6 class="center-align">{{ message }}</h6>
     <div class="row">
+      <label>{{ labelText }}</label>
       <input type="text" v-model="message">
     </div>
   </div>
@@ -9,6 +10,7 @@
 
 <script>
 export default {
+  props: ["labelText"],
   data: function() {
     return {
       message: "This is the child component"
