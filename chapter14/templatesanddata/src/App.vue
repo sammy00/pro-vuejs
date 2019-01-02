62ad58b94e6e8a818cc04575ae833dad20e58321
sammy00 <xiangminli@outlook.com>
2019-01-02 08:31:20 +0800

    Listing 14-10. Handling Events in the App.vue

--- chapter14/templatesanddata/src/App.vue
+++ chapter14/templatesanddata/src/App.vue
@@ -1,62 +1,22 @@
 <template>
   <div class="container">
-    <div class="blue card-panel white-text">
-      <h3 class="center-align">{{ message }}</h3>
+    <div id="outer-element" class="teal card-panel white-text" @click="handleClick">Outer Element
+      <div id="middle-element" class="card-panel grey" @click="handleClick">Middle Element
+        <div id="inner-element" class="card-panel light-blue" @click="handleClick">Inner Element</div>
+      </div>
     </div>
-    <table class="striped">
-      <thead>
-        <tr>
-          <th>Index</th>
-          <th>Name</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(name, index) in names" v-bind:key="name">
-          <td>{{index}}</td>
-          <td>{{name}}</td>
-          <td>
-            <button
-              class="btn btn-sm bg-primary white-text"
-              v-on="buttonEvents"
-              :data-name="name"
-            >Select</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: "MyComponent",
-  data: function() {
-    return {
-      buttonEvents: {
-        click: this.handleClick,
-        mousemove: this.handleMouseEvent,
-        mouseleave: this.handleMouseEvent
-      },
-      counter: 0,
-      message: "Ready",
-      names: ["Kayak", "Lifejacket", "Soccer Ball", "Stadium"]
-    };
-  },
   methods: {
     handleClick($event) {
-      let name = $event.target.dataset.name;
-      this.message = `Select: ${name}`;
-    },
-    handleMouseEvent($event) {
-      let name = $event.target.dataset.name;
-
-      if ($event.type == "mousemove") {
-        this.message = `Move in ${name} ${this.counter++}`;
-      } else {
-        this.counter = 0;
-        this.message = "Ready";
-      }
+      console.log(
+        `handleClick target: ${$event.target.id}` +
+          ` currentTarget: ${$event.currentTarget.id}`
+      );
     }
   }
 };
