82d8b72d57fb96c9de1a1423b72e00ff8edcfd7c
sammy00 <xiangminli@outlook.com>
2019-01-02 13:54:30 +0800

    Listing 17-6. Replacing the Contents of the App.vue

---
+++ chapter17/lifecycles/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="card-panel">
+      <div class="row">
+        <label>
+          <input type="checkbox" v-model="checked">
+          <span>Checkbox</span>
+        </label>
+      </div>
+      Checked Value: {{ checked }}
+    </div>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  //components: {
+  //  HelloWorld
+  //}
+  data() {
+    return {
+      checked: true
+    };
+  }
+};
+</script>
