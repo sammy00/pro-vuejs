3487d592eb448e7bd29fc7ffd622fb256705d400
sammy00 <xiangminli@outlook.com>
2019-01-02 09:00:01 +0800

    Listing 15-1. Simplifying the Content of the App.vue

---
+++ chapter15/templatesanddata/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="card-panel teal white-text">Value: {{ dataValue }}</div>
+    <form>
+      <div class="row">
+        <label>
+          <input type="checkbox" @change="handleChange">
+          <span>Data Value</span>
+        </label>
+      </div>
+      <div class="row">
+        <button class="btn grey" type="reset" @click="reset">Reset</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      dataValue: false
+    };
+  },
+  methods: {
+    handleChange($event) {
+      this.dataValue = $event.target.checked;
+    },
+    reset() {
+      this.dataValue = false;
+    }
+  }
+};
+</script>
