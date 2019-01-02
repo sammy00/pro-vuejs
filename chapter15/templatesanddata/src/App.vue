9d04eea02d9ac1123a2f14004566c22ffbf407e7
sammy00 <xiangminli@outlook.com>
2019-01-02 12:11:45 +0800

    Listing 15-4. Expanding the Form Elements in the App.vue

--- chapter15/templatesanddata/src/App.vue
+++ chapter15/templatesanddata/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="container">
-    <div class="card-panel teal white-text">Value: {{ dataValue }}</div>
+    <div class="card-panel teal white-text">
+      <div>Value: {{ dataValue }}</div>
+      <div>Other Value: {{ otherValue || "(Empty)" }}</div>
+    </div>
+
     <form>
       <div class="row">
         <label>
           <input type="checkbox" @change="handleChange" :checked="dataValue">
           <span>Data Value</span>
         </label>
+      </div>
+      <div class="input-field">
+        <input type="text" @input="handleChange" :value="otherValue">
       </div>
       <div class="row">
         <button class="btn grey" type="reset" @click="reset">Reset</button>
@@ -20,12 +27,17 @@ export default {
   name: "MyComponent",
   data() {
     return {
-      dataValue: false
+      dataValue: false,
+      otherValue: ""
     };
   },
   methods: {
     handleChange($event) {
-      this.dataValue = $event.target.checked;
+      if ($event.target.type == "checkbox") {
+        this.dataValue = $event.target.checked;
+      } else {
+        this.otherValue = $event.target.value;
+      }
     },
     reset() {
       this.dataValue = false;
