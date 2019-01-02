04bdc966fef05d6f74e1fd7b182d36f3209af711
sammy00 <xiangminli@outlook.com>
2019-01-02 08:13:36 +0800

    Listing 14-1. Simplifying the Content of the App.vue

---
+++ chapter14/templatesanddata/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <div class="blue card-panel white-text">
+      <h3 class="center-align">{{ name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data: function() {
+    return {
+      name: "Lifejacket"
+    };
+  }
+};
+</script>
