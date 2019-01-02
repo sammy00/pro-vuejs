e9d4073dc498fedacd0073fd5b08a9d85ffe3345
sammy00 <xiangminli@outlook.com>
2019-01-02 13:10:36 +0800

    Listing 16-4. Delegating to a Child Component in the App.vue

---
+++ chapter16/templatesanddata/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="card-panel blue">Root Component
+      <ChildComponent></ChildComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChildComponent from "./components/Child";
+
+export default {
+  name: "MyComponent",
+  components: {
+    ChildComponent
+  }
+};
+</script>
---
+++ chapter16/templatesanddata/src/components/Child.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="card-panel teal white-text">
+    <h6 class="center-align">Child Component</h6>
+  </div>
+</template>
