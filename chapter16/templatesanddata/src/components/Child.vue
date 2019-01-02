528e47ed8b728c87b1b6c7531571c6580b9d309b
sammy00 <xiangminli@outlook.com>
2019-01-02 13:14:28 +0800

    Listing 16-7. Adding Features in the Child.vue

--- chapter16/templatesanddata/src/components/Child.vue
+++ chapter16/templatesanddata/src/components/Child.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="card-panel blue white-text">
-    <h6 class="center-align">Child Component</h6>
+    <h6 class="center-align">{{ message }}</h6>
+    <div class="row">
+      <input type="text" v-model="message">
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "This is the child component"
+    };
+  }
+};
+</script>
