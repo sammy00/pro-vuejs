30519bb0738b740b8d8c76aa03cd1c7f0f568157
sammy00 <xiangminli@outlook.com>
2019-01-02 17:43:21 +0800

    Listing 17-15. The Contents of the MessageDisplay.vue

---
+++ chapter17/lifecycles/src/components/MessageDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-panel grey darken-1 light-text">
+    <div>Counter Value: {{ counter }}</div>
+    <button class="btn btn-grey" @click="handleClick">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  created: function() {
+    console.log("MessageDisplay: created");
+  },
+  beforeDestroy: function() {
+    console.log("MessageDisplay: beforeDestroy");
+  },
+  destroyed: function() {
+    console.log("MessageDisplay: destroyed");
+  },
+  methods: {
+    handleClick() {
+      this.counter++;
+    }
+  }
+};
+</script>
