8b71d750b9e14217171d3ff859e4b0039ebc4d43
sammy00 <xiangminli@outlook.com>
2019-01-04 07:53:22 +0800

    Listing 22-6. Using URL Routing in the App.vue

--- chapter22/productapp/src/App.vue
+++ chapter22/productapp/src/App.vue
@@ -1,59 +1,16 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row center">
-        <button class="btn" @click="selectComponent('table')">Standard Features</button>
-        &nbsp;
-        <button class="btn green" @click="selectComponent('summary')">Advanced</button>
-      </div>
       <div class="row">
-        <!-- eslint-disable-next-line -->
-        <component :is="selectedComponent"></component>
+        <router-view></router-view>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ErrorDisplay from "./components/ErrorDisplay";
-import LoadingMessage from "./components/LoadingMessage";
-import ProductDisplay from "./components/ProductDisplay";
-import ProductEditor from "./components/ProductEditor";
-
-const DataSummary = () => ({
-  component: import("./components/DataSummary"),
-  loading: LoadingMessage,
-  delay: 100
-});
-
-import { mapMutations, mapState } from "vuex";
-
 export default {
   name: "App",
-  // eslint-disable-next-line
-  components: { DataSummary, ErrorDisplay, ProductDisplay, ProductEditor },
-  computed: {
-    ...mapState({
-      selected: state => state.nav.selected
-    }),
-    selectedComponent() {
-      switch (this.selected) {
-        case "table":
-          return ProductDisplay;
-        case "editor":
-          return ProductEditor;
-        case "summary":
-          return DataSummary;
-      }
-
-      return null;
-    }
-  },
-  methods: {
-    ...mapMutations({
-      selectComponent: "nav/selectComponent"
-    })
-  },
   created() {
     this.$store.dispatch("getProductsAction");
   }
