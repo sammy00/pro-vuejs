346d49972892a37d5ba6a66d62fa540b9e4da973
sammy00 <xiangminli@outlook.com>
2019-01-03 13:08:50 +0800

    Listing 20-6. The Contents of the index.js File in the src/store

---
+++ chapter20/productapp/src/store/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    products: [
+      {
+        id: 1,
+        name: 'Product #1',
+        category: 'Test',
+        price: 100,
+      },
+      {
+        id: 2,
+        name: 'Product #2',
+        category: 'Test',
+        price: 150,
+      },
+      {
+        id: 3,
+        name: 'Product #3',
+        category: 'Test',
+        price: 200,
+      },
+    ],
+  },
+  mutations: {
+    saveProduct(currentState, product) {
+      let index = currentState.products.findIndex((p) => p.id == product.id)
+      if (index == -1) {
+        currentState.products.push(product)
+      } else {
+        Vue.set(currentState.products, index, product)
+      }
+    },
+    deleteProduct(currentState, product) {
+      let index = currentState.products.findIndex((p) => p.id == product.id)
+      currentState.products.splice(index, 1)
+    },
+  },
+})
