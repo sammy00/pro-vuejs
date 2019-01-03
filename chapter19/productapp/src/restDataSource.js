6ad2b35a28bf61ad4670589cd46b3ff4a276ea88
sammy00 <xiangminli@outlook.com>
2019-01-03 12:41:43 +0800

    Listing 19-20. Handling Errors in the restDataSource.js

--- chapter19/productapp/src/restDataSource.js
+++ chapter19/productapp/src/restDataSource.js
@@ -3,16 +3,38 @@ import Axios from 'axios'
 const baseUrl = 'http://localhost:3500/products/'
 
 export class RestDataSource {
-  async deleteProduct(product) {
-    await Axios.delete(`${baseUrl}${product.id}`, product)
+  constructor(bus) {
+    this.eventBus = bus
   }
   async getProducts() {
-    return (await Axios.get(baseUrl)).data
+    return (await this.sendRequest('GET', baseUrl)).data
   }
   async saveProduct(product) {
-    await Axios.post(baseUrl, product)
+    await this.sendRequest('POST', baseUrl, product)
   }
   async updateProduct(product) {
-    await Axios.put(`${baseUrl}${product.id}`, product)
+    await this.sendRequest('PUT', `${baseUrl}${product.id}`, product)
+  }
+  async deleteProduct(product) {
+    await this.sendRequest('DELETE', `${baseUrl}${product.id}`, product)
+  }
+  async sendRequest(httpMethod, url, product) {
+    try {
+      return await Axios.request({
+        method: httpMethod,
+        url: url,
+        data: product,
+      })
+    } catch (err) {
+      if (err.response) {
+        this.eventBus.$emit(
+          'httpError',
+          `${err.response.statusText} - ${err.response.status}`
+        )
+      } else {
+        this.eventBus.$emit('httpError', 'HTTP Error')
+      }
+      throw err
+    }
   }
 }
