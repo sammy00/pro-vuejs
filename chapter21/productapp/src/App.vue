1f2f1312be323593350128682b8aa11f2ce8ac0e
sammy00 <xiangminli@outlook.com>
2019-01-03 18:01:09 +0800

    Listing 21-7. Using a Data Binding in the App.vue

--- chapter21/productapp/src/App.vue
+++ chapter21/productapp/src/App.vue
@@ -4,10 +4,30 @@
       <div class="row">
         <error-display/>
       </div>
+      <div class="row">
+        <label>
+          <input
+            type="radio"
+            value="table"
+            v-model="selected"
+            :class="{active: (selected == 'table')}"
+          >
+          <span>Table</span>
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="editor"
+            v-model="selected"
+            :class="{active: (selected == 'table')}"
+          >
+          <span>Editor</span>
+        </label>
+      </div>
 
       <div class="row">
         <!-- eslint-disable-next-line -->
-        <component is="ProductDisplay"></component>
+        <component :is="selectedComponent"></component>
       </div>
     </div>
   </div>
@@ -22,6 +42,16 @@ export default {
   name: "App",
   // eslint-disable-next-line
   components: { ErrorDisplay, ProductDisplay, ProductEditor },
+  data: function() {
+    return {
+      selected: "table"
+    };
+  },
+  computed: {
+    selectedComponent() {
+      return this.selected == "table" ? ProductDisplay : ProductEditor;
+    }
+  },
   created() {
     this.$store.dispatch("getProductsAction");
   }
