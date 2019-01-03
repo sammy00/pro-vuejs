be1d1aaeb77bd12fdab70732584579685e22ca7f
sammy00 <xiangminli@outlook.com>
2019-01-04 07:49:44 +0800

    Listing 22-1. Installing the Routing Package

---
+++ chapter22/productapp/src/components/ProductDisplay.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="row">
+    <table :class="'striped' == useStripedTable">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Price</th>
+        <th></th>
+      </tr>
+      <tbody>
+        <tr v-for="p in products" :key="p.id">
+          <td>{{ p.id }}</td>
+          <td>{{ p.name }}</td>
+          <td>{{ p.category }}</td>
+          <td>{{ p.price }}</td>
+          <td>
+            <button class="btn btn-small" :class="editClass" @click="editProduct(p)">Edit</button>
+            &nbsp;
+            <button
+              class="btn btn-small"
+              :class="deleteClass"
+              @click="deleteProduct(p)"
+            >Delete</button>
+          </td>
+        </tr>
+        <tr v-if="products.length == 0">
+          <td colspan="5" class="text-center">No Data</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="row center">
+      <button class="btn" @click="createNew">Create New</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({
+      tableClass: "prefs/tableClass",
+      editClass: "prefs/editClass",
+      deleteClass: "prefs/deleteClass"
+    }),
+    ...mapState(["products"]),
+    ...mapState({
+      useStripedTable: state => state.prefs.stripedTable
+    })
+  },
+  methods: {
+    ...mapActions({
+      deleteProduct: "deleteProductAction"
+    }),
+    ...mapMutations({
+      selectProduct: "selectProduct",
+      selectComponent: "nav/selectComponent",
+      setEditButtonColor: "prefs/setEditButtonColor",
+      setDeleteButtonColor: "prefs/setDeleteButtonColor"
+    }),
+    createNew() {
+      this.selectProduct();
+      this.selectComponent("editor");
+    },
+    editProduct(product) {
+      this.selectProduct(product);
+      this.selectComponent("editor");
+    }
+  },
+  created() {
+    //this.getProducts();
+    this.setEditButtonColor(false);
+    this.setDeleteButtonColor(false);
+  }
+};
+</script>
---
+++ chapter22/productapp/src/components/ProductEditor.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="row">
+      <input v-model="product.id">
+      <label>ID</label>
+    </div>
+    <div class="row">
+      <input v-model="product.name">
+      <label>Name</label>
+    </div>
+    <div class="row">
+      <input v-model="product.category">
+      <label>Category</label>
+    </div>
+    <div class="row">
+      <input v-model="product.price">
+      <label>Price</label>
+    </div>
+
+    <div class="row">
+      <button class="btn" @click="save">{{ editing ? "Save" : "Create" }}</button>
+      &nbsp;
+      <button class="btn grey" @click="cancel">Cancel</button>
+    </div>
+  </div>
+</template>
+
+<script>
+let unwatcher;
+
+export default {
+  data() {
+    return {
+      editing: false,
+      product: {}
+    };
+  },
+  methods: {
+    cancel() {
+      this.$store.commit("selectProduct");
+      this.$store.commit("nav/selectComponent", "table");
+    },
+    async save() {
+      this.$store.dispatch("saveProductAction", this.product);
+      this.$store.commit("nav/selectComponent", "table");
+
+      this.product = {};
+    },
+    selectProduct(selectedProduct) {
+      if (selectedProduct == null) {
+        this.editing = false;
+        this.product = {};
+      } else {
+        this.editing = true;
+        this.product = {};
+        Object.assign(this.product, selectedProduct);
+      }
+    }
+  },
+  beforeDestroy() {
+    unwatcher();
+  },
+  created() {
+    unwatcher = this.$store.watch(
+      state => state.selectedProduct,
+      this.selectProduct
+    );
+    this.selectProduct(this.$store.state.selectedProduct);
+  }
+};
+</script>
+
+<!--Listing 18-8. The Contents of the ProductEditor.vue-->
