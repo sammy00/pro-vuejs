a82677b30d018b1f5b9ebac9cf8eceb1c7223802
sammy00 <xiangminli@outlook.com>
2019-01-04 12:04:18 +0800

    Listing 24-11. Route Guarding in the Preferences.vue

--- chapter24/productapp/src/components/Preferences.vue
+++ chapter24/productapp/src/components/Preferences.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="card-panel center" v-if="displayWarning">
+      <h5>Are you sure?</h5>
+      <button class="btn red" @click="doNavigation">Yes</button> &nbsp;
+      <button class="btn red" @click="cancelNavigation">Cancel</button>
+    </div>
     <h4 class="card-panel teal white-text center-align">Preferences</h4>
     <div class="row">
       <label>
@@ -19,6 +24,13 @@
 <script>
 import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      displayWarning: false,
+      navigationApproved: false,
+      targetRoute: null
+    };
+  },
   computed: {
     ...mapState({
       primaryEdit: state => state.prefs.primaryEditButton,
@@ -26,11 +38,28 @@ export default {
     })
   },
   methods: {
-    setPrimaryEdit() {
-      this.$store.commit("prefs/setEditButtonColor", !this.primaryEdit);
+    cancelNavigation() {
+      this.navigationApproved = false;
+      this.displayWarning = false;
+    },
+    doNavigation() {
+      this.navigationApproved = true;
+      this.$router.push(this.targetRoute.path);
     },
     setDangerDelete() {
       this.$store.commit("prefs/setDeleteButtonColor", !this.dangerDelete);
+    },
+    setPrimaryEdit() {
+      this.$store.commit("prefs/setEditButtonColor", !this.primaryEdit);
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.navigationApproved) {
+      next();
+    } else {
+      this.targetRoute = to;
+      this.displayWarning = true;
+      next(false);
     }
   }
 };
