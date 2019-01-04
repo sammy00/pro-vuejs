71e47612a46c1ca84980b4b8cc1d29f903d02582
sammy00 <xiangminli@outlook.com>
2019-01-04 13:19:40 +0800

    Listing 25-8. The Contents of the ListMakerControls.vue

---
+++ chapter25/transitions/src/components/ListMakerControls.vue
@@ -0,0 +1,36 @@
+<template>
+  <tfoot>
+    <tr v-if="showAdd">
+      <td></td>
+      <td>
+        <input class="form-control" v-model="currentItem">
+      </td>
+      <td>
+        <button id="add" class="btn btn-sm btn-info" v-on:click="handleAdd">Add</button>
+        <button id="cancel" class="btn btn-sm btn-secondary" v-on:click="showAdd = false">Cancel</button>
+      </td>
+    </tr>
+    <tr v-else>
+      <td colspan="4" class="text-center p-2">
+        <button class="btn btn-info" v-on:click="showAdd = true">Show Add</button>
+      </td>
+    </tr>
+  </tfoot>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      showAdd: false,
+      currentItem: ""
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.$emit("add", this.currentItem);
+      this.showAdd = false;
+    }
+  }
+};
+</script>
