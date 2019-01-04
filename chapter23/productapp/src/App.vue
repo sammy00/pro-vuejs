e2bdaff53d0dac0c9fb4ed0a76ed5efa3f292ca7
sammy00 <xiangminli@outlook.com>
2019-01-04 08:38:11 +0800

    Listing 23-5. Specifying Tag Type in the App.vue

--- chapter23/productapp/src/App.vue
+++ chapter23/productapp/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div class="container">
       <div class="row center">
+        <!--
         <ul class="tabs">
           <li class="tab">
             <router-link to="/list">List</router-link>
@@ -10,6 +11,11 @@
             <router-link to="/create">Create</router-link>
           </li>
         </ul>
+        -->
+        <ol>
+          <router-link tag="li" to="/list">List</router-link>
+          <router-link tag="li" to="/create">Create</router-link>
+        </ol>
       </div>
       <div class="row">
         <router-view></router-view>
