f15e39e18e777f8f713b4a3845a8f58979da27ad
sammy00 <xiangminli@outlook.com>
2019-01-04 13:37:33 +0800

    Listing 25-15. Adding an Element in the SimpleDisplay.vue

--- chapter25/transitions/src/components/SimpleDisplay.vue
+++ chapter25/transitions/src/components/SimpleDisplay.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="card-panel">
     <h3 class="card-panel orange center-align white-text">Display</h3>
-    <transition enter-to-class="fadeIn" leave-to-class="fadeOut">
-      <h4 v-if="show" class="animated card-panel teal center white-text">Hello, Adam</h4>
+    <transition enter-active-class="fadeIn" leave-active-class="fadeOut" mode="out-in">
+      <h4 v-if="show" class="animated card-panel teal center white-text" key="hello">Hello, Adam</h4>
+      <h4 v-else class="animated card-panel center green white-text" key="goodbye">Goodbye, Adam</h4>
     </transition>
     <div class="row center">
       <button class="blue btn" @click="toggle">Toggle Visibility</button>
