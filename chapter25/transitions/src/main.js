35e266fddd05bb5afdc85c802421deb8342a4973
sammy00 <xiangminli@outlook.com>
2019-01-04 13:16:28 +0800

    Listing 25-4. Incorporating the Packages in the main.js

--- chapter25/transitions/src/main.js
+++ chapter25/transitions/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'animate.css/animate.min.css'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'popmotion/dist/popmotion.global.min.js'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
