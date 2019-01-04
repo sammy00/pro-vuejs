e84184b3274c7fc4af0dbb84d5abf5dc20722621
sammy00 <xiangminli@outlook.com>
2019-01-04 13:48:15 +0800

    Listing 25-19. Handling Transition Events in the ListMakerControls.vue

--- chapter25/transitions/src/components/ListMakerControls.vue
+++ chapter25/transitions/src/components/ListMakerControls.vue
@@ -1,20 +1,27 @@
 <template>
   <tfoot>
-    <tr v-if="showAdd">
-      <td></td>
-      <td>
-        <input class="form-control" v-model="currentItem">
-      </td>
-      <td>
-        <button id="add" class="btn btn-sm btn-info" v-on:click="handleAdd">Add</button>
-        <button id="cancel" class="btn btn-sm btn-secondary" v-on:click="showAdd = false">Cancel</button>
-      </td>
-    </tr>
-    <tr v-else>
-      <td colspan="4" class="text-center p-2">
-        <button class="btn btn-info" v-on:click="showAdd = true">Show Add</button>
-      </td>
-    </tr>
+    <transition v-on:beforeEnter="beforeEnter" v-on:after-enter="afterEnter" mode="out-in">
+      <tr v-if="showAdd" key="addCancel">
+        <td></td>
+        <td>
+          <input type="text" v-model="currentItem">
+        </td>
+        <td>
+          <button id="add" class="btn btn-small" @click="handleAdd">Add</button>
+          &nbsp;
+          <button
+            id="cancel"
+            class="btn btn-small grey"
+            @click="showAdd = false"
+          >Cancel</button>
+        </td>
+      </tr>
+      <tr v-else key="show">
+        <td colspan="4" class="text-center p-2">
+          <button class="btn btn-info" v-on:click="showAdd = true">Show Add</button>
+        </td>
+      </tr>
+    </transition>
   </tfoot>
 </template>
 
@@ -27,6 +34,14 @@ export default {
     };
   },
   methods: {
+    afterEnter(el) {
+      el.classList.remove("animated", "fadeIn");
+    },
+    beforeEnter(el) {
+      if (this.showAdd) {
+        el.classList.add("animated", "fadeIn");
+      }
+    },
     handleAdd() {
       this.$emit("add", this.currentItem);
       this.showAdd = false;
