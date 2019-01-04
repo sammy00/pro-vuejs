37f803adf6d5cc2089db9923d5287c7ac42b34ee
sammy00 <xiangminli@outlook.com>
2019-01-04 13:57:52 +0800

    Listing 25-21. Responding to Changes in the Numbers.vue

--- chapter25/transitions/src/components/Numbers.vue
+++ chapter25/transitions/src/components/Numbers.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="mx-5 p-2 border border-dark">
-    <h3 class="bg-success text-white text-center p-2">Numbers</h3>
+  <div class="card-panel">
+    <h3 class="card-panel green white-text center-align">Numbers</h3>
     <div class="container-fluid">
       <div class="row">
-        <div class="col">
-          <input class="form-control" v-model.number="first">
+        <div class="col m1">
+          <input v-model.number="first">
         </div>
-        <div class="col-1 h3">+</div>
-        <div class="col">
-          <input class="form-control" v-model.number="second">
+        <div class="col m1">
+          <h5>+</h5>
         </div>
-        <div class="col h3">= {{ total }}</div>
+        <div class="col m1">
+          <input v-model.number="second">
+        </div>
+        <div class="col m2">
+          <h5>= {{ displayTotal }}</h5>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { tween } from "popmotion";
+
 export default {
   data: function() {
     return {
       first: 10,
-      second: 20
+      second: 20,
+      displayTotal: 30
     };
   },
   computed: {
     total() {
       return this.first + this.second;
+    }
+  },
+  watch: {
+    total(newVal, oldVal) {
+      let t = tween({
+        from: Number(oldVal),
+        to: Number(newVal),
+        duration: 250
+      });
+
+      // eslint-disable-next-line
+      t.start(val => (this.displayTotal = val.toFixed(0)));
     }
   }
 };
