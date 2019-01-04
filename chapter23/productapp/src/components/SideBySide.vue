7f19f785d167b2836ea35d32f7726730cf02050d
sammy00 <xiangminli@outlook.com>
2019-01-04 11:41:17 +0800

    Listing 23-15. The Contents of the SideBySide.vue

---
+++ chapter23/productapp/src/components/SideBySide.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col m6">
+        <h3 class="grey white-text center-align">Left View</h3>
+        <router-view name="left" class="card-panel grey"/>
+      </div>
+      <div class="col m6">
+        <h3 class="grey white-text center-align">Right View</h3>
+        <router-view name="right" class="card-panel grey"/>
+      </div>
+    </div>
+  </div>
+</template>
