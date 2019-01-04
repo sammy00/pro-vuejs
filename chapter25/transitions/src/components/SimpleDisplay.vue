f43504bd9105246c81efb68b7e1cc888839d628b
sammy00 <xiangminli@outlook.com>
2019-01-04 13:32:22 +0800

    Listing 25-13. Applying a Transition in the SimpleDisplay.vue

--- chapter25/transitions/src/components/SimpleDisplay.vue
+++ chapter25/transitions/src/components/SimpleDisplay.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card-panel">
     <h3 class="card-panel orange center-align white-text">Display</h3>
-    <h4 v-if="show" class="card-panel teal center white-text">Hello, Adam</h4>
+    <transition>
+      <h4 v-if="show" class="card-panel teal center white-text">Hello, Adam</h4>
+    </transition>
     <div class="row center">
       <button class="blue btn" @click="toggle">Toggle Visibility</button>
     </div>
@@ -22,3 +24,20 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-leave-active {
+  opacity: 0;
+  font-size: 0em;
+  transition: all 250ms;
+}
+.v-enter {
+  opacity: 0;
+  font-size: 0em;
+}
+.v-enter-to {
+  opacity: 1;
+  font-size: x-large;
+  transition: all 250ms;
+}
+</style>
