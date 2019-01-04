3ef2595030754432b8edeaf5cfa62b40b3e0c1b2
sammy00 <xiangminli@outlook.com>
2019-01-04 08:49:24 +0800

    Listing 23-13. Defining Routes in the index.js File in the src/router Folder

--- chapter23/productapp/src/router/index.js
+++ chapter23/productapp/src/router/index.js
@@ -1,21 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Preferences from '../components/Preferences'
 import ProductDisplay from '../components/ProductDisplay'
 import ProductEditor from '../components/ProductEditor'
+import Products from '../components/Products'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history',
   routes: [
-    { name: 'table', path: '/', component: ProductDisplay, alias: '/list' },
+    { path: '/preferences', component: Preferences },
     {
-      name: 'editor',
-      path: '/:op(create|edit)/:id(\\d+)?',
-      component: ProductEditor,
+      path: '/products',
+      component: Products,
+      children: [
+        { name: 'table', path: 'list', component: ProductDisplay },
+        {
+          name: 'editor',
+          path: ':op(create|edit)/:id(\\d+)?',
+          component: ProductEditor,
+        },
+        { path: '', redirect: 'list' },
+      ],
     },
-
-    { path: '*', redirect: '/' },
+    { path: '/edit/:id', redirect: (to) => `/products/edit/${to.params.id}` },
+    { path: '*', redirect: '/products/list' },
   ],
 })
