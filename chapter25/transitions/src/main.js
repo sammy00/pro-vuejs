a554e587c23706ce960c768ce7360a96a2fe98f7
sammy00 <xiangminli@outlook.com>
2019-01-04 13:21:07 +0800

    Listing 25-10. Enabling Routing in the main.js

--- chapter25/transitions/src/main.js
+++ chapter25/transitions/src/main.js
@@ -5,8 +5,11 @@ import 'animate.css/animate.min.css'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'popmotion/dist/popmotion.global.min.js'
 
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount('#app')
