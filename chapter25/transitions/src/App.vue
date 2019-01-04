d5d6da53dc588a79c82375c86b829b1436325108
sammy00 <xiangminli@outlook.com>
2019-01-04 13:22:01 +0800

    Listing 25-11. Adding Navigation Elements in the App.vue

--- chapter25/transitions/src/App.vue
+++ chapter25/transitions/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-center m-2">
+      <div class="btn-group">
+        <router-link
+          tag="button"
+          to="/display"
+          exact-active-class="btn-warning"
+          class="btn btn-secondary"
+        >Simple Display</router-link>
+        <router-link
+          tag="button"
+          to="/list"
+          exact-active-class="btn-info"
+          class="btn btn-secondary"
+        >List Maker</router-link>
+        <router-link
+          tag="button"
+          to="/numbers"
+          exact-active-class="btn-success"
+          class="btn btn-secondary"
+        >Numbers</router-link>
+      </div>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
