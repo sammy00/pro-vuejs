8170c6369cb7e1e7f1127519aa59ce5abb321725
sammy00 <xiangminli@outlook.com>
2019-01-04 13:17:15 +0800

    Listing 25-5. The Contents of the SimpleDisplay.vue

---
+++ chapter25/transitions/src/components/SimpleDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mx-5 border border-dark p-2">
+    <h3 class="bg-warning text-white text-center p-2">Display</h3>
+    <div v-if="show" class="h4 bg-info text-center p-2">Hello, Adam</div>
+    <div class="text-center">
+      <button class="btn btn-primary" v-on:click="toggle">Toggle Visibility</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      show: true
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    }
+  }
+};
+</script>
