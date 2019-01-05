227491afbceaf29d58cc9a7c5b63ec7e81376b60
sammy00 <xiangminli@outlook.com>
2019-01-05 10:34:07 +0800

    Listing 26-17. Using a Single Function in the colorizer.js

--- chapter26/extendingvue/src/directives/colorize.js
+++ chapter26/extendingvue/src/directives/colorize.js
@@ -1,16 +1,7 @@
-export default {
-  bind(el, binding) {
-    if (binding.value) {
-      el.classList.add('red', 'white-text')
-    } else {
-      el.classList.remove('red', 'white-text')
-    }
-  },
-  update(el, binding) {
-    if (binding.value) {
-      el.classList.add('red', 'white-text')
-    } else {
-      el.classList.remove('red', 'white-text')
-    }
-  },
+export default function(el, binding) {
+  if (binding.value) {
+    el.classList.add('red', 'white-text')
+  } else {
+    el.classList.remove('red', 'white-text')
+  }
 }
