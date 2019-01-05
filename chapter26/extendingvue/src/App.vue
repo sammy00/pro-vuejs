06b609f405a2fd9e19cfd188106dc91e4f74f07e
sammy00 <xiangminli@outlook.com>
2019-01-05 10:39:58 +0800

    Listing 26-22. Adding a Component in the App.vue

--- chapter26/extendingvue/src/App.vue
+++ chapter26/extendingvue/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app" class="container">
     <numbers/>
+    <subtraction/>
   </div>
 </template>
 
 <script>
 import Numbers from "./components/Numbers";
+import Subtraction from "./components/Subtraction";
 
 export default {
   name: "app",
   components: {
-    Numbers
+    Numbers,
+    Subtraction
   }
 };
 </script>
