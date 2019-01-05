38aa295ecece206f918e76153118f2697c90295e
sammy00 <xiangminli@outlook.com>
2019-01-05 10:31:56 +0800

    Listing 26-15. Using Data Attributes in the colorize.js

--- chapter26/extendingvue/src/directives/colorize.js
+++ chapter26/extendingvue/src/directives/colorize.js
@@ -3,15 +3,24 @@ export default {
     const bgClass = binding.arg || 'red'
     const noMods = Object.keys(binding.modifiers).length == 0
 
-    if (!binding.value) {
-      return el.classList.remove(bgClass, 'white-text')
-    }
-
-    if (noMods || binding.modifiers.bg) {
-      el.classList.add(bgClass)
-    }
-    if (noMods || binding.modifiers.text) {
-      el.classList.add('white-text')
+    if (binding.value) {
+      if (noMods || binding.modifiers.bg) {
+        el.classList.add(bgClass)
+        el.dataset['bgClass'] = true
+      }
+      if (noMods || binding.modifiers.text) {
+        el.classList.add('white-text')
+        el.dataset['textClass'] = true
+      }
+    } else {
+      if (el.dataset['bgClass']) {
+        el.classList.remove(bgClass)
+        el.dataset['bgClass'] = false
+      }
+      if (el.dataset['textClass']) {
+        el.classList.remove('white-text')
+        el.dataset['textClass'] = false
+      }
     }
   },
 }
