cbe5c6df755ab2f0166d3484e4675598b4d94f7d
sammy00 <xiangminli@outlook.com>
2019-01-05 10:33:17 +0800

    Listing 26-16. Adding a Hook in the colorizer.js

--- chapter26/extendingvue/src/directives/colorize.js
+++ chapter26/extendingvue/src/directives/colorize.js
@@ -1,26 +1,16 @@
 export default {
-  update(el, binding) {
-    const bgClass = binding.arg || 'red'
-    const noMods = Object.keys(binding.modifiers).length == 0
-
+  bind(el, binding) {
     if (binding.value) {
-      if (noMods || binding.modifiers.bg) {
-        el.classList.add(bgClass)
-        el.dataset['bgClass'] = true
-      }
-      if (noMods || binding.modifiers.text) {
-        el.classList.add('white-text')
-        el.dataset['textClass'] = true
-      }
+      el.classList.add('red', 'white-text')
     } else {
-      if (el.dataset['bgClass']) {
-        el.classList.remove(bgClass)
-        el.dataset['bgClass'] = false
-      }
-      if (el.dataset['textClass']) {
-        el.classList.remove('white-text')
-        el.dataset['textClass'] = false
-      }
+      el.classList.remove('red', 'white-text')
+    }
+  },
+  update(el, binding) {
+    if (binding.value) {
+      el.classList.add('red', 'white-text')
+    } else {
+      el.classList.remove('red', 'white-text')
     }
   },
 }
